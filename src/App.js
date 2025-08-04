8784e479c7bb2b1b29d05b1e5a1dd7a55a521116
MehdiBouchachi <mehdibch.dev@gmail.com>
2025-08-04 20:13:05 +0200

    fixing bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import { useState } from "react";
+import { NavBar } from "./NavBar";
+import { Box, MoviesList } from "./Box";
+import { Logo } from "./NavBar";
+import { NumResults } from "./NavBar";
+import { Search } from "./NavBar";
+import { Summary } from "./Summary";
+import { SummaryList } from "./SummaryList";
+import { MovieDetails } from "./MovieDetails";
+import { useMovie } from "./useMovie";
+import { useLocalStorageState } from "./useLocalStorageState";
 
+/*
 const tempMovieData = [
   {
     imdbID: "tt1375666",
@@ -45,124 +56,80 @@ const tempWatchedData = [
     imdbRating: 8.5,
     userRating: 9,
   },
-];
+];*/
+export const KEY = "fa154d1a";
 
-const average = (arr) =>
+export const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
   const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
-  const [isOpen1, setIsOpen1] = useState(true);
-  const [isOpen2, setIsOpen2] = useState(true);
+  const [selectedId, setSelectedId] = useState(null);
+  const { movies, isloading, error } = useMovie(query);
+  const [watched, setWatched] = useLocalStorageState([], "watched");
 
-  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-  const avgUserRating = average(watched.map((movie) => movie.userRating));
-  const avgRuntime = average(watched.map((movie) => movie.runtime));
+  function handleSelectMovie(id) {
+    setSelectedId((selectedId) => (id === selectedId ? null : id));
+  }
+  function handleCloseMovie() {
+    setSelectedId(null);
+  }
 
+  function handleMovieWatched(movie) {
+    setWatched((watched) => [...watched, movie]);
+  }
+  function handleDeletWtached(id) {
+    setWatched((watched) => watched.filter((movie) => movie.imbdId !== id));
+  }
+
+  //fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`).then((res)=> res.json()).then(data=>console.log(data.Search))
   return (
     <>
-      <nav className="nav-bar">
-        <div className="logo">
-          <span role="img">🍿</span>
-          <h1>usePopcorn</h1>
-        </div>
-        <input
-          className="search"
-          type="text"
-          placeholder="Search movies..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <p className="num-results">
-          Found <strong>{movies.length}</strong> results
-        </p>
-      </nav>
+      <NavBar movies={movies}>
+        <Logo />
+        <Search query={query} setQuery={setQuery} />
+        <NumResults movies={movies} />
+      </NavBar>
 
-      <main className="main">
-        <div className="box">
-          <button
-            className="btn-toggle"
-            onClick={() => setIsOpen1((open) => !open)}
-          >
-            {isOpen1 ? "–" : "+"}
-          </button>
-          {isOpen1 && (
-            <ul className="list">
-              {movies?.map((movie) => (
-                <li key={movie.imdbID}>
-                  <img src={movie.Poster} alt={`${movie.Title} poster`} />
-                  <h3>{movie.Title}</h3>
-                  <div>
-                    <p>
-                      <span>🗓</span>
-                      <span>{movie.Year}</span>
-                    </p>
-                  </div>
-                </li>
-              ))}
-            </ul>
+      <Main>
+        <Box>
+          {/*isloading ? <Loader /> : <MoviesList movies={movies} />*/}
+
+          {isloading && <Loader />}
+          {!isloading && !error && (
+            <MoviesList movies={movies} OnSelectedMovie={handleSelectMovie} />
           )}
-        </div>
-
-        <div className="box">
-          <button
-            className="btn-toggle"
-            onClick={() => setIsOpen2((open) => !open)}
-          >
-            {isOpen2 ? "–" : "+"}
-          </button>
-          {isOpen2 && (
+          {error && <ErrorMsg msg={error} />}
+        </Box>
+        <Box>
+          {selectedId ? (
+            <MovieDetails
+              selectedId={selectedId}
+              onCloseMovie={handleCloseMovie}
+              onAddMoviesWatched={handleMovieWatched}
+              Watched={watched}
+            />
+          ) : (
             <>
-              <div className="summary">
-                <h2>Movies you watched</h2>
-                <div>
-                  <p>
-                    <span>#️⃣</span>
-                    <span>{watched.length} movies</span>
-                  </p>
-                  <p>
-                    <span>⭐️</span>
-                    <span>{avgImdbRating}</span>
-                  </p>
-                  <p>
-                    <span>🌟</span>
-                    <span>{avgUserRating}</span>
-                  </p>
-                  <p>
-                    <span>⏳</span>
-                    <span>{avgRuntime} min</span>
-                  </p>
-                </div>
-              </div>
-
-              <ul className="list">
-                {watched.map((movie) => (
-                  <li key={movie.imdbID}>
-                    <img src={movie.Poster} alt={`${movie.Title} poster`} />
-                    <h3>{movie.Title}</h3>
-                    <div>
-                      <p>
-                        <span>⭐️</span>
-                        <span>{movie.imdbRating}</span>
-                      </p>
-                      <p>
-                        <span>🌟</span>
-                        <span>{movie.userRating}</span>
-                      </p>
-                      <p>
-                        <span>⏳</span>
-                        <span>{movie.runtime} min</span>
-                      </p>
-                    </div>
-                  </li>
-                ))}
-              </ul>
+              <Summary watched={watched} />
+              <SummaryList
+                watched={watched}
+                onDeleteWtached={handleDeletWtached}
+              />
             </>
           )}
-        </div>
-      </main>
+        </Box>
+      </Main>
     </>
   );
+}
+function Main({ children }) {
+  return <main className="main">{children}</main>;
+}
+
+export function ErrorMsg({ msg }) {
+  return <p className="error">⛔{msg}</p>;
+}
+export function Loader() {
+  return <p className="loader">Loading...</p>;
 }
